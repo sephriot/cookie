e09e14b85f6e9c836727c454fd76846dd9f6aadb
Jakub Wojtowicz <wojtjk@gmail.com>
2021-02-26 01:06:36 +0100

    Initial commmit

---
+++ cookie.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CookieApp(App):
+
+    def build(self):
+        return Widget()
+
+
+if __name__ == '__main__':
+    CookieApp().run()
