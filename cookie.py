85e2b2294aa5e0751014253c5f439cb4432592fa
Jakub Wojtowicz <wojtjk@gmail.com>
2021-03-10 21:30:04 +0100

    Implemented basic layout

--- cookie.py
+++ cookie.py
@@ -1,11 +1,16 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+
+
+class Cookie(FloatLayout):
+    pass
 
 
 class CookieApp(App):
 
     def build(self):
-        return Widget()
+        return Cookie()
 
 
 if __name__ == '__main__':
