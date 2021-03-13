2907602c5840508a184c77a2358cc9a801f741c7
Jakub Wojtowicz <wojtjk@gmail.com>
2021-03-13 21:16:47 +0100

    Implemented drawing

--- cookie.py
+++ cookie.py
@@ -8,13 +8,26 @@ from pictureview import PictureView
 
 class Cookie(FloatLayout):
 
+    def enable_draw_mode(self, mode):
+        self.ids.picture.draw_mode = mode
+        self.ids.picture_view.scroll_timeout = 1
+        self.ids.picture_view.scroll_distance = 100000
+
+    def disable_draw_mode(self):
+        self.ids.picture_view.scroll_timeout = 200
+        self.ids.picture_view.scroll_distance = 20
+        self.ids.picture.draw_mode = ""
+
     def shape_button_click(self, button):
         next_state = not button.selected
         self.deactivate_buttons()
         button.selected = next_state
         print(button.name)
+        if next_state:
+            self.enable_draw_mode(button.name)
 
     def deactivate_buttons(self):
+        self.disable_draw_mode()
         for key in self.ids:
             if isinstance(self.ids[key], ShapeButton):
                 self.ids[key].selected = False
--- picture.py
+++ picture.py
@@ -1,13 +1,77 @@
-from kivy.properties import StringProperty
+import math
+
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.instructions import InstructionGroup
+from kivy.graphics.vertex_instructions import Line
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.scatter import Scatter
 
 
 class Picture(Scatter):
     source = StringProperty()
+    draw_mode = StringProperty("")
+    last_drawing = ObjectProperty()
 
     def on_touch_down(self, touch):
         if touch.button == "scrolldown":
             self.scale *= 1.25
         elif touch.button == "scrollup":
             self.scale *= 0.8
-        return super(Picture, self).on_touch_down(touch)
+        elif not self.draw_mode:
+            return super(Picture, self).on_touch_down(touch)
+
+        self.scale_touch(touch)
+        touch.ud['group'] = InstructionGroup()
+        with self.canvas:
+            touch.ud['group'].add(Color(1, 1, 0))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+            touch.ud['line'].points += [touch.x, touch.y]
+            if self.draw_mode == "freeform":
+                touch.ud['line'].close = True
+
+    def on_touch_up(self, touch):
+        if not self.draw_mode:
+            return super(Picture, self).on_touch_up(touch)
+
+        self.scale_touch(touch)
+
+        if self.canvas.indexof(touch.ud['line']) != -1:
+            self.canvas.remove(touch.ud['line'])
+        if self.draw_mode == "circle":
+            self.circle(touch)
+        elif self.draw_mode == "rectangle":
+            self.rectangle(touch)
+
+        if self.canvas.indexof(touch.ud['group']) != -1:
+            self.canvas.remove(touch.ud['group'])
+        else:
+            touch.ud['group'].add(touch.ud['line'])
+            self.canvas.add(touch.ud['group'])
+
+        self.last_drawing = touch.ud['group']
+        pass
+
+    def on_touch_move(self, touch):
+        self.scale_touch(touch)
+        if not self.draw_mode:
+            return super(Picture, self).on_touch_move(touch)
+        if self.draw_mode != "freeform":
+            del touch.ud['line'].points[-2:]
+        touch.ud['line'].points += [touch.x, touch.y]
+
+    def scale_touch(self, touch):
+        touch.x /= self.scale
+        touch.y /= self.scale
+
+    def circle(self, touch):
+        point = touch.ud['line'].points
+        touch.ud['line'] = Line(circle=(
+            (point[0] + point[2]) / 2, (point[1] + point[3]) / 2,
+            math.dist([point[0], point[1]], [point[2], point[3]]) / 2))
+
+    def rectangle(self, touch):
+        point = touch.ud['line'].points
+        touch.ud['line'] = Line(rectangle=(
+            min(point[0], point[2]), min(point[1], point[3]),
+            max(point[0], point[2]) - min(point[0], point[2]),
+            max(point[1], point[3]) - min(point[1], point[3])))
